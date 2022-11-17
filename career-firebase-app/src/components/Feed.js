10ad988bf8bed2c136f5663f817832bb6fe6ccd8
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-11-17 20:19:27 +0530

    Career

---
+++ career-firebase-app/src/components/Feed.js
@@ -0,0 +1,62 @@
+import { CalendarViewDay, Create, EventNote, Image, Subscriptions } from '@material-ui/icons'
+import React, { useEffect, useState } from 'react'
+import './Feed.css'
+import InputOption from './InputOption'
+import Post from './Post'
+import { db } from '../firebase'
+import firebase from "firebase";
+
+const Feed = () => {
+    const [posts, setPosts] = useState([])
+    const [input, setInput] = useState('')
+
+    useEffect(() => {
+        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        })
+    },[])
+
+    const sendPost = e => {
+        e.preventDefault()
+        db.collection('posts').add({
+            name: 'Nabendu Biswas',
+            description: 'This is a test',
+            message: input,
+            photoUrl: '',
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setInput('')
+    }
+    return (
+        <div className='feed'>
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <Create />
+                    <form onSubmit={sendPost}>
+                        <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={Image} title="Photo" color="#70B5F9" /> <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
+                    <InputOption Icon={EventNote} title="Event" color="#C0CBCD" /> <InputOption Icon={CalendarViewDay} title="Write Article"
+                        color="#7FC15E" />
+                </div>
+            </div>
+            {posts.map(({ id, data}) => (
+                <Post 
+                    key={id}
+                    name={data.name} 
+                    description={data.description} 
+                    message={data.message} 
+                    photoUrl={data.photoUrl}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default Feed
---
+++ career-firebase-app/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_-cW3URoobb4cPH07QlKIMzv6qoIS0vc",
+    authDomain: "tinder-firebase-skygoal.firebaseapp.com",
+    projectId: "tinder-firebase-skygoal",
+    storageBucket: "tinder-firebase-skygoal.appspot.com",
+    messagingSenderId: "780306896000",
+    appId: "1:780306896000:web:3c45f29365a116edf9c265"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth, db }
